4f8cc0f03665503613e4184ef7c7f696be013784
Ferny <fernc28@outlook.com>
2023-04-20 18:30:05 -0400

    public route

--- controllers/public_routes.js
+++ controllers/public_routes.js
@@ -1,29 +1,29 @@
-const router = require ('express').Router;
+const router = require('express').Router;
 // const { User, House } = require('../model');
 
 
 /// Middleware function to redirect a logged in user to the dashboard
 // We don't want to show auth pages if they're logged in
 function isLoggedIn(req, res, next) {
-    if (req.session.user_id) return res.redirect('/dashboard');
+    if (!req.session.user_id) return res.redirect('/login');
 
     next();
-  }
-  
-  // Render the Homepage view
-  router.get('/', isLoggedIn, (req, res) => {
-    res.render('index');
-  });
-  
-  // Render the Login Page view
-  router.get('/login', isLoggedIn, (req, res) => {
-    res.render('auth/login');
-  });
-  
+}
 
-  // Render the Register Page view
-  router.get('/register', isLoggedIn, (req, res) => {
-    res.render('auth/register');
-  });
-  
-  module.exports = router;
+// Render the Homepage view
+router.get('/', isLoggedIn, (req, res) => {
+    res.render('dashboard');
+});
+
+// Render the Login Page view
+router.get('/login', (req, res) => {
+    res.render('index');
+});``
+
+
+// Render the Register Page view
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+module.exports = router;
