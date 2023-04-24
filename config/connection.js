02ce88bbd74874a260cecb574a9b60f2fdcbe2c4
Ferny <fernc28@outlook.com>
2023-04-24 10:43:07 -0400

    heroku

--- config/connection.js
+++ config/connection.js
@@ -9,5 +9,5 @@ const connection = !PORT ? new Sequelize(
     host: '127.0.0.1',
     dialect: 'mysql'
   }
-) : new Sequelize(process.env.JAWS_DB_URL);
+) : new Sequelize("mysql://x2h2xa2ceclb7uj7:u8lsk6by4jahdc02@exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/emunqlq3i5hs5iph");
 module.exports = connection;
