80a203c8c51150654370a7f7fda9bcd258a264fc
Ferny <fernc28@outlook.com>
2023-04-24 10:36:14 -0400

    ready for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-
-const connection = new Sequelize(
+const PORT = process.env.PORT;
+const connection = !PORT ? new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
@@ -9,6 +9,5 @@ const connection = new Sequelize(
     host: '127.0.0.1',
     dialect: 'mysql'
   }
-);
-
+) : new Sequelize(process.env.JAWS_DB_URL);
 module.exports = connection;
